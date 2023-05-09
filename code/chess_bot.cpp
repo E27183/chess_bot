d0bb9a3e0726d7cb2058b3084ecd702b10b312a7
Edan <edan.landow@gmail.com>
2023-05-09 23:41:37 +1000

    pointers are cool

--- code/chess_bot.cpp
+++ code/chess_bot.cpp
@@ -4,20 +4,104 @@
 using namespace std;
 
 struct moveList {
-    char from;
-    char to;
+    char from[2];
+    char to[2];
     moveList* next;
 };
 
 struct moveListToOnly {
-    char to;
+    char to[2];
     moveListToOnly* next;
 };
 
-moveListToOnly diagonalMove[64][4] = {};
-moveListToOnly straightMove[64][4] = {};
-moveListToOnly knightMove[64] = {};
-moveListToOnly kingMove[64] = {};
+struct fourMoveListToHeap {
+    moveListToOnly* first;
+    moveListToOnly* second;
+    moveListToOnly* third;
+    moveListToOnly* fourth;
+};
+
+moveListToOnly diagonalMove[8][8][4] = {};
+moveListToOnly straightMove[8][8][4] = {};
+moveListToOnly knightMove[8][8] = {};
+moveListToOnly kingMove[8][8] = {};
+
+fourMoveListToHeap* diagonalM(char* from) {
+    char position_x = *from;
+    char position_y = *(from + 1);
+    fourMoveListToHeap output;
+    output.first = NULL;
+    output.second = NULL;
+    output.third = NULL;
+    output.fourth = NULL;
+    moveListToOnly* prev = NULL;
+    while (position_x > 0 && position_y > 0) {
+        position_x--;
+        position_y--;
+        moveListToOnly node;
+        node.to[0] = position_x;
+        node.to[1] = position_y;
+        node.next = NULL;
+        if (prev != NULL) {
+            (*prev).next = &node;
+        } else {
+            output.first = &node;
+        };
+        prev = &node;
+    };
+    char position_x = *from;
+    char position_y = *(from + 1);
+    moveListToOnly* prev = NULL;
+    while (position_x > 0 && position_y < 8) {
+        position_x--;
+        position_y++;
+        moveListToOnly node;
+        node.to[0] = position_x;
+        node.to[1] = position_y;
+        node.next = NULL;
+        if (prev != NULL) {
+            (*prev).next = &node;
+        } else {
+            output.second = &node;
+        };
+        prev = &node;
+    };
+    char position_x = *from;
+    char position_y = *(from + 1);
+    moveListToOnly* prev = NULL;
+    while (position_x < 8 && position_y > 0) {
+        position_x++;
+        position_y--;
+        moveListToOnly node;
+        node.to[0] = position_x;
+        node.to[1] = position_y;
+        node.next = NULL;
+        if (prev != NULL) {
+            (*prev).next = &node;
+        } else {
+            output.third = &node;
+        };
+        prev = &node;
+    };
+    char position_x = *from;
+    char position_y = *(from + 1);
+    moveListToOnly* prev = NULL;
+    while (position_x < 8 && position_y < 8) {
+        position_x++;
+        position_y++;
+        moveListToOnly node;
+        node.to[0] = position_x;
+        node.to[1] = position_y;
+        node.next = NULL;
+        if (prev != NULL) {
+            (*prev).next = &node;
+        } else {
+            output.fourth = &node;
+        };
+        prev = &node;
+    };
+    return &output;
+};
 
 class gameState {
     public:
@@ -37,7 +121,7 @@ class gameState {
         1x - White
         2x - Black
         */
-       char board[64];
+       char board[8][8];
        double time_remaining;
 };
 
